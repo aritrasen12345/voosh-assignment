ffcebc2504186ba30e6bf5f8051ffc798056aee9
Aritra Sen <aritrasen12345@gmail.com>
2024-05-23 22:29:16 +0530

    added validationErrorHandler

---
+++ src/middlewares/validationErrorHandler.js
@@ -0,0 +1,30 @@
+import { validationResult } from "express-validator";
+
+/**
+ * This is an error handling middleware
+ * If there is any validation error this middleware handle that otherwise proceed normally
+ */
+
+const validationErrorHandler = (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      /**
+       * If there is nay error then throwing error along with details.
+       */
+      throw new Error(`Field validation failed!`, {
+        cause: {
+          indicator: "validation",
+          status: 400,
+          details: errors,
+        },
+      });
+    }
+
+    // * If there is no error
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default validationErrorHandler;
